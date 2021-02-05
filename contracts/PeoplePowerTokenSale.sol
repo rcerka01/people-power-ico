a736107d1ff112dc0443d0e364f421aaa6f60343
Ray <raitis.cerkasovs@gmail.com>
2021-02-05 00:19:22 +0000

    Add buy tokens

--- contracts/PeoplePowerTokenSale.sol
+++ contracts/PeoplePowerTokenSale.sol
@@ -7,11 +7,28 @@ contract PeoplePowerTokenSale {
 
     address admin;
     uint256 public tokenPrice;
+    uint256 public tokensSold;
     PeoplePowerToken public tokenContract;
 
     constructor (PeoplePowerToken _tokenContract, uint256 _tokenPrice) public {
         admin = msg.sender;
         tokenContract = _tokenContract;
         tokenPrice = _tokenPrice;
+    }
+
+    event Sell(address _buyer, uint256 _amount);
+
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x  * y) / y == x);
+    }
+
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
+
+        tokensSold += _numberOfTokens;
+
+        emit Sell(msg.sender, _numberOfTokens);
     }
 }
--- test/PeoplePowerTokenSale.js
+++ test/PeoplePowerTokenSale.js
@@ -1,9 +1,16 @@
+const PeoplePowerToken = artifacts.require("PeoplePowerToken");
 const PeoplePowerTokenSale = artifacts.require("PeoplePowerTokenSale");
 
 contract('PeoplePowerTokenSale',  function(accounts) {
 
+    var tokenInstance;
     var tokenSaleInstance;
     var setPrice = 1000000;
+    var admin = accounts[0];
+    var buyer = accounts[1];
+    var numberOfTokens = 10;
+    var tokensAvailable = 50000000000;
+    var tokensUnavailable = tokensAvailable + 1;
 
     it('Initialise sale contract', function() {
         return PeoplePowerTokenSale.deployed()
@@ -11,13 +18,49 @@ contract('PeoplePowerTokenSale',  function(accounts) {
                 tokenSaleInstance = instance;
                 return tokenSaleInstance.address;
             }).then(function(address){
-                assert.notEqual(address, 0x0, 'Has contract address')
+                assert.notEqual(address, 0x0, 'has contract address')
                 return tokenSaleInstance.tokenContract();
             }).then(function(address){
-                assert.notEqual(address, 0x0, 'Has token contract address')
+                assert.notEqual(address, 0x0, 'has token contract address')
                 return tokenSaleInstance.tokenPrice()
             }).then(function(price) {
-                assert.equal(price, setPrice, 'Returns correct token price')
+                assert.equal(price, setPrice, 'returns correct token price')
             })
+    })
+
+    it('Can buy tokens', function() { 
+        return PeoplePowerToken.deployed()
+            .then(function(instance) {
+                tokenInstance = instance;
+                return PeoplePowerTokenSale.deployed()
+            }).then(function(instance) {
+                tokenSaleInstance = instance;
+                return tokenInstance.transfer(tokenSaleInstance.address, tokensAvailable, { from: admin });
+            }).then(function(receipt) {
+                let value = numberOfTokens * setPrice;
+                return tokenSaleInstance.buyTokens(numberOfTokens, {from: buyer, value: value});
+            }).then(function(receipt){
+                assert.equal(receipt.logs.length, 1, 'one event');
+                assert.equal(receipt.logs[0].event, 'Sell', 'Sell event exist');
+                assert.equal(receipt.logs[0].args._buyer, buyer, 'logs sender');
+                assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs amount');
+                return tokenSaleInstance.tokensSold();
+            }).then(function(amount) {
+                assert.equal(amount.toNumber(), numberOfTokens, 'set correct amount of tokens sold')
+                return tokenInstance.balanceOf(buyer);
+            }).then(function(balance) {
+                assert.equal(balance.toNumber(), numberOfTokens, 'buyer balance increments correctly')
+                return tokenInstance.balanceOf(tokenSaleInstance.address);
+            }).then(function(balance) {
+                assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens), 'selers balance decrements correctly';
+                let value = tokensUnavailable * setPrice;
+                return tokenSaleInstance.buyTokens(tokensUnavailable, { from: buyer, value: value });
+            }).then(assert.fail).catch(function(error) {
+                assert(error.message.indexOf('revert') >= 0, "can not buy more tokens than available")
+                return tokenSaleInstance.buyTokens(numberOfTokens, {from: buyer, value: 1});   
+            })
+            .then(assert.fail).catch(function(error) {
+                assert(error.message.indexOf('revert') >= 0, "amount sent cannot be less than token price") 
+            })            
     })
 });
